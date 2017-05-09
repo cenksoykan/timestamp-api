dd7f0f8f9f20f088c3e15c93f020bb092f55673d
Cenk SOYKAN <cenk.soykan@yahoo.com>
2017-05-09 12:33:40 +0000

    Add moment.js dependency

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 var express = require('express')
 var app = express()
+var moment = require('moment')
+var path = require('path')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var port = process.env.PORT || 8080
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/:query', function(req,res) {
+  var stamp
+  if(/^\d{8,}$/.test(req.params.query)) {
+    stamp = moment(req.params.query, "X")
+  } else {
+    stamp = moment(req.params.query, "MMMM D, YYYY")
+  }
+
+  if(stamp.isValid()) {
+    res.json({
+      unix: stamp.format("X"),
+      natural: stamp.format("MMMM D, YYYY")
+    })
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    })
+  }
 })
